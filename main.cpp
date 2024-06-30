a68173457c50d24f9aa104174e552870f439d61f
Ji-Siqi <deserverest2@gmail.com>
2024-06-30 18:19:46 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
+#include <QTcpServer>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    QTextStream qin(stdin);
+    QTextStream qout(stdout);
+    QTcpServer tcpServer;
+    qout << "Please enter the room number you want to create.(It's four figures.)" << endl;
+    int port;
+    qin >> port;
+    tcpServer.listen(QHostAddress::Any, port);
+    qout << "OK." << endl << "Please enter the number of people in the room.(It's a positive integer and it's a single digit number.)" << endl;
+    int peopleNumber;
 
     return a.exec();
 }
